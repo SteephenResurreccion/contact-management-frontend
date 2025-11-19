5c18a545fc649f9e3b7ed97f6a6e50f18aedf17f
Reymark De Castro <reymark_decastro@dlsl.edu.ph>
2025-11-20 00:09:00 +0800

    Last

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -47,8 +47,7 @@ export default function LandingPage() {
         {/* LEFT TEXT */}
         <div className="landing-vignette">
           <h1 className="landing-vignette-title">
-            Welcome to<br />
-            <span className="landing-brand-accent">PeoplePad</span>
+            Welcome to <span className="landing-brand-accent">PeoplePad</span>
           </h1>
 
           <p className="landing-vignette-text">
